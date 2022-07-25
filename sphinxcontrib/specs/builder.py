e579973b76921cfd0459c2bf4ac01420c5bee3a7
Ashley Trinh <ashley@hackbrightacademy.com>
2022-07-25 11:06:36 -0700

    WIP

--- sphinxcontrib/specs/builder.py
+++ sphinxcontrib/specs/builder.py
@@ -3,9 +3,17 @@
 The Specializations builder.
 """
 
+from typing import Dict, Any
+
+from docutils import nodes
 from sphinx.builders.html import StandaloneHTMLBuilder
 
 
 class SpecsBuilder(StandaloneHTMLBuilder):
     name = "specs"
     search = False
+
+    # def get_doc_context(
+    #     self, docname: str, body: str, metatags: str
+    # ) -> Dict[str, Any]:
+    #     return super().get_doc_context(docname, body, metatags)
