9796a359a6d3924663048aedbf1042616abb9a8f
Ashley Trinh <ashley@hackbrightacademy.com>
2022-08-03 17:15:09 -0700

    Test build with sphinxlectern

--- tests/roots/test-builder-example/conf.py
+++ tests/roots/test-builder-example/conf.py
@@ -1,7 +1,35 @@
-extensions = ["sphinxcontrib.specs"]
+extensions = ["sphinxcontrib.specs", "sphinxlectern"]
 html_theme = "specs"
 html_use_index = False
 html_sidebars = {"**": []}
 html_permalinks_icon = "#"
-smartquotes = False
+smartquotes = True
 pygments_style = "default"
+
+lectern_substitutions = {
+    # Python executable & version
+    "pyname": "Python 3",
+    "py": "python3",
+    "pyi": "`python3`",
+    "pycmd": "`python3`:cmd:",
+    # iPython
+    "ipyname": "IPython",
+    "ipy": "ipython3",
+    "ipyi": "`ipython3`",
+    "ipycmd": "`ipython3`:cmd:",
+    # PIP
+    "pipname": "Pip 3",
+    "pip": "pip3",
+    "pipi": "`pip3`",
+    "pipcmd": "`pip3`:cmd:",
+    # Virtualenv manager
+    "venvname": "Virtualenv",
+    "venv": "virtualenv",
+    "venvi": "`virtualenv`",
+    "venvcmd": "`virtualenv`:cmd:",
+    # Text editor
+    "editorname": "VS Code",
+    "editor": "code",
+    "editori": "`code`",
+    "editcmd": "`code`:cmd:",
+}
